ee1c3a4afd091a9215b16cb499ae7e3e5e338856
Taro <wangys@xipudata.com>
2019-06-20 16:45:11 +0800

    增加show-size

--- README.md
+++ README.md
@@ -1,17 +1,35 @@
-# vue-my-carousel
+# carousel
 
-> 轮播图，可设置多张同时显示
+> A Vue.js project
 
-## 插件使用方法：
+## Build Setup
 
-
-```
-#安装插件
+``` bash
+# 安装插件
 npm i vue-my-carousel
 
-#在main.js引入插件
+# 在main.js中引入
 import MyCarousel from 'vue-my-carousel'
-Vue.use(MyCarousel)
-```
+Vue.use(MyCarousel);
 
+# 直接调用
+<my-carousel :autoplay="true" :height="200" :interval="5000" :show-size="3" :carousel-list="dataList"></my-carousel>
+```
+```
+参数说明：
+autoplay 为true时自动播放，默认向右
+height 轮播的高度
+interval 轮播的时间间隔
+show-size 轮播一页显示几个
+carousel-list 轮播的内容
+格式如下：
+[
+{
+ url: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1519175636,579560806&fm=27&gp=0.jpg',//图片地址
+ link: 'https://www.baidu.com',//后续做跳转
+}
+]
+
+
+```
 For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
--- src/lib/MyCarousel.vue
+++ src/lib/MyCarousel.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="my-carousel" :style="'height:'+height+'px;'">
-    <div v-for="(item,index) in carouselListData" :key="index" class="my-carousel-item">
+    <div v-for="(item,index) in carouselListData" :key="index" class="my-carousel-item" :style="'transform: translateX('+(100/showSize) * index+'vw)'">
       <img :src="item.url" alt="img"/>
     </div>
     <div class="arrow arrow-left" @click="next()">
@@ -16,7 +16,7 @@
 <script>
   export default {
     name: "my-carousel",
-    props: ['carouselList', 'interval', 'height', 'autoplay'],
+    props: ['carouselList', 'interval', 'height', 'autoplay','showSize'],
     data() {
       return {
         carouselListData: [],
@@ -57,47 +57,61 @@
   }
 </script>
 
-<style lang="stylus">
-  .my-carousel
-    position: relative
-    width: 100%
-    overflow: hidden
-    for row in 1 2 3 4 5
-      .my-carousel-item:nth-child({row})
-        transform: translateX(33.333vw * row)
-  .my-carousel-item
-    width: 33.33%
-    background: #ccc
-    position: absolute
-    height: 100%
-    overflow: hidden
-    transition: transform 3s linear
-    img
-      width: 100%
-  .arrow
-    color: #fff
-    position: absolute
-    z-index: 999
-    top: 50%
-    margin-top: -20px
-    i
-      height: 40px
-      width: 40px
-      border-radius: 100%
-      background: rgba(0, 0, 0, .1)
-      line-height: 40px
-      box-sizing: border-box
-  .arrow.arrow-left
-    left: 0
-    margin-left: -20px
-    text-align: right
-    i
-      padding-right: 6px
-  .arrow.arrow-right
-    right: 0
-    margin-right: -20px
-    text-align: left
-    i
-      padding-left: 6px
+<style scoped>
+  .my-carousel {
+    position: relative;
+    width: 100%;
+    overflow: hidden;
+  }
+
+  .my-carousel-item {
+    width: 33.33%;
+    background: #ccc;
+    position: absolute;
+    height: 100%;
+    overflow: hidden;
+    transition: transform 3s linear;
+  }
+
+  img {
+    width: 100%;
+  }
+
+  .arrow {
+    color: #fff;
+    position: absolute;
+    z-index: 999;
+    top: 50%;
+    margin-top: -20px;
+  }
+
+  i {
+    height: 40px;
+    width: 40px;
+    border-radius: 100%;
+    background: rgba(0, 0, 0, .1);
+    line-height: 40px;
+    box-sizing: border-box;
+  }
+
+  .arrow.arrow-left {
+    left: 0;
+    margin-left: -20px;
+    text-align: right;
+  }
+
+  i {
+    padding-right: 6px
+  }
+
+  .arrow.arrow-right {
+    right: 0;
+    margin-right: -20px;
+    text-align: left;
+  }
+
+  i {
+    padding-left: 6px;
+  }
 
 </style>
